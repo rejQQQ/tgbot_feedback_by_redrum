61882b7806a62097c0697a235a1c34d1d4a847e0
TheeeWhiteDeath <144166856+TheeeWhiteDeath@users.noreply.github.com>
2025-01-13 15:33:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-API_TOKEN = '6462181247:AAHyliLrztCqXHLMfxdX4pngszMSbpl3oto' # bot token
-admin = [1350650566] # id админа (взять можно тут - t.me/userinfobot)
+API_TOKEN = '6462181247' # bot token
+admin = [
+
+] # id админа (взять можно тут - t.me/userinfobot)
